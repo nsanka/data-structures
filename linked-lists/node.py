ec2dc3569d147448bd28eab87a3431ca7af6778e
Naga Sanka <nsanka@me.com>
2024-05-15 17:50:37 -0400

    Define linked list class and updated node class

--- linked-lists/node.py
+++ linked-lists/node.py
@@ -1,24 +1,25 @@
 # Define Node class below:
 class Node:
-  """
-  Build Node class that can have Data of any type and link to Next Node
-  """
-  def __init__(self, value, next_node=None):
-    self.value = value
-    self.next_node = next_node
+   """
+   Build Node class that can have Data of any type and link to Next Node
+   """
+   def __init__(self, value, next_node=None):
+      self.value = value
+      self.next_node = next_node
 
-  def get_value(self):
-    """Return the value stored for the Node"""
-    return self.value
+   def get_value(self):
+      """Return the value stored for the Node"""
+      return self.value
 
-  def get_next_node(self):
-    """Return the link to Next Node"""
-    return self.next_node
+   def get_next_node(self):
+      """Return the link to Next Node"""
+      return self.next_node
 
-  def set_next_node(self, next_node):
-    """Update the link to Next Node"""
-    self.next_node = next_node
+   def set_next_node(self, next_node):
+      """Update the link to Next Node"""
+      self.next_node = next_node
 
 
-my_node = Node(44)
-print(my_node.get_value())
+if __name__ == "__main__":
+   my_node = Node(44)
+   print(my_node.get_value())

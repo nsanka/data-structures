82f37d02536cc2e7e9eef50ad04d3d28e5d473bc
Naga Sanka <nsanka@me.com>
2024-05-15 14:51:29 -0400

    Create node.py

---
+++ linked-lists/node.py
@@ -0,0 +1,24 @@
+# Define Node class below:
+class Node:
+  """
+  Build Node class that can have Data of any type and link to Next Node
+  """
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    """Return the value stored for the Node"""
+    return self.value
+
+  def get_next_node(self):
+    """Return the link to Next Node"""
+    return self.next_node
+
+  def set_next_node(self, next_node):
+    """Update the link to Next Node"""
+    self.next_node = next_node
+
+
+my_node = Node(44)
+print(my_node.get_value())
